e23cae9a4f6f75de9949b619c54efcdbb6e2c255
mrmqs159 <mrmqs159@gmail.com>
2022-09-01 19:08:25 +0200

    Add project files.

---
+++ BeruTask/Shared/GoldPriceDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeruTask.Shared
+{
+    public class GoldPriceDto
+    {
+        public double StartPrice { get; set; }
+
+        public double EndPrice { get; set; }
+
+        public double AvgPrice { get; set; }
+    }
+}
